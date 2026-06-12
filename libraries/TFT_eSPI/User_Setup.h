cd9049fad616a2292878746517e64cdef8929a8c
Eli <elispiro@gmail.com>
2026-06-11 22:03:57 -0400

    Modify TFT_eSPI User_Setup.h for display settings
    Updated display configuration and added font options.

--- libraries/TFT_eSPI/User_Setup.h
+++ libraries/TFT_eSPI/User_Setup.h
@@ -1,24 +1,30 @@
-#define USER_SETUP_INFO "User_Setup"
-#define ILI9341_2_DRIVER
-#define TFT_WIDTH  240
-#define TFT_HEIGHT 320
+// --- ECHCYD RECOMMENDED CONFIG FOR TFT_eSPI ---
+// Copy this content into: /home/eli/Arduino/libraries/TFT_eSPI/User_Setup.h
 
-// --- The Corrected CYD Pins ---
+#define USER_SETUP_ID 1
+
+// --- Driver ---
+#define ILI9341_2_DRIVER     // Most CYDs use this version of the ILI9341
+
+// --- Display Pins (Standard CYD) ---
 #define TFT_MISO 12
 #define TFT_MOSI 13
 #define TFT_SCLK 14
-#define TFT_CS   15
-#define TFT_DC   2
-#define TFT_RST  -1  
-#define TFT_BL   21  
-// ------------------------------
+#define TFT_CS   15  // Display Chip Select
+#define TFT_DC    2  // Data/Command
+#define TFT_RST  -1  // Set to -1 if connected to ESP32 RST pin
+#define TFT_BL   21  // Backlight pin (matches what we used in code)
 
-#define TFT_BACKLIGHT_ON HIGH  
-#define LOAD_GLCD
-#define LOAD_FONT2
-#define LOAD_FONT4
-#define SPI_FREQUENCY  55000000
+// --- Fonts ---
+// Enabling these will ensure drawString/drawNumber with font IDs work
+#define LOAD_GLCD   // Font 1. Original Adafruit 8 pixel font
+#define LOAD_FONT2  // Font 2. Small 16 pixel high font
+#define LOAD_FONT4  // Font 4. Medium 26 pixel high font
+#define LOAD_FONT6  // Font 6. Large 48 pixel font
+#define LOAD_FONT7  // Font 7. 7 segment 48 pixel font
+#define LOAD_FONT8  // Font 8. Large 75 pixel font
+#define LOAD_GFXFF  // FreeFonts.
+
+// --- Bus Speed ---
+#define SPI_FREQUENCY  40000000
 #define SPI_READ_FREQUENCY  20000000
-#define SPI_TOUCH_FREQUENCY  2500000
-
-#define TOUCH_CS 21     // Chip select pin (T_CS) of touch screen
